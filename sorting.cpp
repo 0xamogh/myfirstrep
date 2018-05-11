559d0670d45df77f887224569753919b22bdc897
Amogh Jahagirdar <31567169+amogh-jrules@users.noreply.github.com>
2018-05-11 23:00:54 +0530

    Create sorting.cpp

---
+++ sorting.cpp
@@ -0,0 +1,12 @@
+void bubbleSort(int array[], int num)
+{
+   int m, n;
+   for (m=0;m< n-1;m++)      
+ 
+           for (n=0;n<num-m-1;n++) 
+           if (arr[n] > arr[n+1]){
+           int temp = arr[n];
+           arr[n] = arr[n+1];
+           arr[n+1] = temp;
+            
+}
